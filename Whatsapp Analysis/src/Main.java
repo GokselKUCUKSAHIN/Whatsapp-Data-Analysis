77c24d89565a18ab92ef476dfee4ba93577124a9
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-11-15 02:23:32 +0300

    Add files via upload

--- Whatsapp Analysis/src/Main.java
+++ Whatsapp Analysis/src/Main.java
@@ -17,24 +17,27 @@ public class Main
         String text = rows.get(41);
         Matcher matcher;
         //                DAY.MONTH.YEAR HOUR:MINUTE - USER_NAME: SOME TEXT FOO BAR BLAH BLAH
-        String regex = "(\\d{2}).(\\d{2}).(\\d{4}) (\\d{2}):(\\d{2}) -([A-Za-z ığüşöçİĞÜŞÖÇ]+): (.*)"; //tested
+        String regex = "(\\d{2}).(\\d{2}).(\\d{4}) (\\d{2}):(\\d{2}) - ([A-Za-z ığüşöçİĞÜŞÖÇ]+): (.*)"; //tested
         Pattern newlinePattern = Pattern.compile(regex);
-        int i = 0;
         for (String line : rows)
         {
             matcher = newlinePattern.matcher(line);
             if (matcher.find())
             {
                 // 0 is whole line
-                /*int day = Integer.parseInt(matcher.group(1));
+                int day = Integer.parseInt(matcher.group(1));
                 int month = Integer.parseInt(matcher.group(2));
                 int year = Integer.parseInt(matcher.group(3));
                 int hour = Integer.parseInt(matcher.group(4));
                 int minute = Integer.parseInt(matcher.group(5));
-                DateTime dt = new DateTime(day,month,year,hour,minute);
+                DateTime dt = new DateTime(day, month, year, hour, minute);
                 //System.out.println(matcher.group(6));*/
-                System.out.println(matcher.group(6));
+                new Message(dt, matcher.group(6), matcher.group(7));
             }
+        }
+        for (Message message:Message.messages)
+        {
+            System.out.println(message.senderID);
         }
     }
 
--- Whatsapp Analysis/src/Message.java
+++ Whatsapp Analysis/src/Message.java
@@ -1,4 +1,54 @@
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
+
+import java.util.ArrayList;
+
 public class Message
 {
+
+    public static ArrayList<String> names = new ArrayList<>();
+    public static ArrayList<Message> messages = new ArrayList<>();
+    //
+    DateTime dateTime;
+    String sender;
+    int senderID;
+    String text;
+
+    //
+    public Message(DateTime dateTime, String sender, String text)
+    {
+        this.dateTime = dateTime;
+        this.sender = sender;
+        this.text = text;
+        checkID();
+        messages.add(this);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%s %s %s", dateTime, sender, text);
+    }
+
+    private void checkID()
+    {
+        if (names.contains(sender))
+        {
+            senderID = names.indexOf(sender);
+        } else
+        {
+            names.add(sender);
+            senderID = names.size() - 1; //last index of list
+        }
+    }
+
+    public String getSender()
+    {
+        return sender;
+    }
+
+    public int getSenderID()
+    {
+        return senderID;
+    }
 
 }
